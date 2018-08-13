bab148298a4f40b2d4d07a045929836b02242925
saashimi <kevin.saavedra@oregonmetro.gov>
2018-08-13 09:08:20 -0700

    Add test code.

---
+++ Grades.Tests/GradeBookTests.cs
@@ -0,0 +1,46 @@
+﻿using Grades;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades.Tests
+{
+    [TestClass]
+    public class GradeBookTests
+    {
+        [TestMethod]
+        public void ComputesHighestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics results = book.ComputeStatistics();
+            Assert.AreEqual(90, results.HighestGrade);
+        }
+
+        public void ComputesLowestGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            GradeStatistics results = book.ComputeStatistics();
+            Assert.AreEqual(10, results.LowestGrade);
+        }
+
+        public void ComputesAverageGrade()
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            GradeStatistics results = book.ComputeStatistics();
+            Assert.AreEqual(85.16, results.AverageGrade, 0.01);
+        }
+    }
+}
